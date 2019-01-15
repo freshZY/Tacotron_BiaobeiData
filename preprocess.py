1459e205f6b65e32dcb8dd71e2495cf529c2e613
admin <zhangyu@DESKTOP-6KDE368.localdomain>
2019-01-15 15:50:39 +0800

    2019.1.15

---
+++ preprocess.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+from multiprocessing import cpu_count
+from tqdm import tqdm
+from datasets import thchs30,biaobei
+from hparams import hparams
+
+
+def preprocess_thchs30(args):
+  in_dir = os.path.join(args.base_dir, 'data_thchs30/data')
+  out_dir = os.path.join(args.base_dir, args.output)
+  os.makedirs(out_dir, exist_ok=True)
+  metadata = thchs30.build_from_path(in_dir, out_dir, args.num_workers, tqdm=tqdm)
+  write_metadata(metadata, out_dir)
+
+def preprocess_biaobei(args):
+  in_dir = os.path.join(args.base_dir, 'biaobei_data/Wave')
+  out_dir = os.path.join(args.base_dir, args.output)
+  os.makedirs(out_dir, exist_ok=True)
+  metadata = biaobei.build_from_path(in_dir, out_dir, args.num_workers, tqdm=tqdm)
+  write_metadata(metadata, out_dir)
+
+def write_metadata(metadata, out_dir):
+  with open(os.path.join(out_dir, 'train.txt'), 'w', encoding='utf-8') as f:
+    for m in metadata:
+      f.write('|'.join([str(x) for x in m]) + '\n')
+  frames = sum([m[2] for m in metadata])
+  hours = frames * hparams.frame_shift_ms / (3600 * 1000)
+  print('Wrote %d utterances, %d frames (%.2f hours)' % (len(metadata), frames, hours))
+  print('Max input length:  %d' % max(len(m[3]) for m in metadata))
+  print('Max output length: %d' % max(m[2] for m in metadata))
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--base_dir', default=os.path.expanduser('.'))
+  parser.add_argument('--output', default='training1')
+  parser.add_argument('--dataset', default='biaobei',choices=['biaobei', 'thchs30'])
+  parser.add_argument('--num_workers', type=int, default=cpu_count())
+  args = parser.parse_args()
+  if args.dataset == 'biaobei':
+    preprocess_biaobei(args)
+  else:
+    preprocess_thchs30(args)
+if __name__ == "__main__":
+  main()
